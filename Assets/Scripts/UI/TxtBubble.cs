ab7119a7aeded11b11106b1f0597333f3c41ab24
mephisst <mephiss@naver.com>
2021-05-13 18:27:38 +0900

    SpeakBubble&AreYouSure&CSV
    Quest, Trenscendence, Karma, Confession csv Added
    Popup_AreYouSure Added
    SpeechBubble Prefabs Added

---
+++ Assets/Scripts/UI/TxtBubble.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TxtBubble : MonoBehaviour
+{
+    [SerializeField] private Image img;
+    [SerializeField] private GameObject tmpGameObject;
+    [SerializeField] private AudioClip ac;
+    private float _interval;
+    private Animator anim;
+
+
+    //이 부분은  dialogue csv 파일의 dialogue001 등으로 대체됨
+    [SerializeField] private Sprite faceSprite;
+    [SerializeField] private string str;
+    [SerializeField] private float f = 0.2f;
+
+    private TextMeshProUGUI tmpro;
+
+    private void Start()
+    {
+        tmpro = tmpGameObject.GetComponent<TextMeshProUGUI>();
+        ChangeValue(faceSprite, str, f);
+        anim = GetComponent<Animator>();
+        PlaySoundEffect();
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            anim.SetTrigger("DieTrigger");
+        }
+    }
+
+    public void WriteText()
+    {
+        img.sprite = faceSprite;
+        TypeWriter.Write(tmpro, str, f);
+    }
+
+    public void ChangeValue(Sprite face, string changeStr, float interval)
+    {
+        img.sprite = face;
+        str = changeStr;
+        _interval = interval;
+    }
+
+    public void Die()
+    {
+        PlaySoundEffect();
+        Destroy(gameObject);
+    }
+
+    public void PlaySoundEffect()
+    {
+        if (ac)
+            SoundEffectManager.SoundEffect(ac);
+    }
+}
